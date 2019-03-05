deb8286c6dd8aa7701bac13ee0768dcef550892f
Damon Brown <dbrow437@gmail.com>
2019-03-04 21:41:37 -0800

    first commit

---
+++ Javascript/exercise3.js
@@ -0,0 +1,11 @@
+// Make a Calculator! using prompt(), and variables, make a program that does the following:
+// 1. Prompts the user for first number.
+// 2. Stores that first number
+// 3. Prompts the user for the second number.
+// 4. stores that number and responds with the SUM by using an alert.
+
+var first = prompt("Input First Number");
+var second = prompt("Input Second Number");
+var add = Number(first) + Number(second);
+alert(sum);
+// BONUS: Make a program that can subtract, multiply, and also divide!
